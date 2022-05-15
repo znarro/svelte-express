446a8a906c628f3543b5f58b4aad00b6a6440d6b
Zamir Narro <znarro@bsale.com.pe>
2022-05-15 15:31:45 -0500

    Call giphy api with axios

--- server.js
+++ server.js
@@ -3,15 +3,31 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
 const path = require("path");
+const axios = require("axios");
 
 app.use(cors());
 
 app.get("/giphy", (req, res) => {
   console.log(`Searching for a gif with the term: ${req.query.term}`);
-  res.send({
-    success: true,
-    data: [],
-  });
+
+  let params = req.query.term.replace(/ /g, "+");
+  params += "&api_key=CkSfNBxJPlR95jPgpMRJfOCFh0VWpV24";
+  params += "&limit=10";
+
+  axios
+    .get(`https://api.giphy.com/v1/gifs/search?q=${params}`)
+    .then((res) => {
+      res.send({
+        success: true,
+        data: response.data.data,
+      });
+    })
+    .catch((error) => {
+      res.send({
+        success: false,
+        data: [],
+      });
+    });
 });
 
 app.use(express.static("public"));
